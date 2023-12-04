b183641ca81888706c005c7096c271932694298a
Harman09 <harmandehal09@gmail.com>
2023-12-04 15:24:09 +0530

    Add new features

--- config.js
+++ config.js
@@ -1,4 +1,18 @@
-<<<<<<< HEAD
+
+// const swaggerUI = require("swagger-ui-express");
+// const YAML = require("yamljs");
+// const swaggerJSDocs = YAML.load("./api2.yaml");
+
+// const options = {
+//     customCss: `img {content:url(\'../logo.svg\'); height:auto;} `,
+//     customfavIcon: "../favicon.ico",
+//     customSiteTitle: "Code Improve API Doc",
+
+// };
+
+//  module.exports = { swaggerServe: swaggerUI.serve, swaggerSetup: swaggerUI.setup(swaggerJSDocs) };
+
+
 const swaggerUI = require("swagger-ui-express");
 const YAML = require("yamljs");
 const swaggerJSDocs = YAML.load("./api2.yaml");
@@ -12,19 +26,5 @@ const options = {
 
  module.exports = { swaggerServe: swaggerUI.serve, swaggerSetup: swaggerUI.setup(swaggerJSDocs) };
 
-=======
-const swaggerUI = require("swagger-ui-express");
-const YAML = require("yamljs");
-const swaggerJSDocs = YAML.load("./api2.yaml");
 
-const options = {
-    customCss: `img {content:url(\'../logo.svg\'); height:auto;} `,
-    customfavIcon: "../favicon.ico",
-    customSiteTitle: "Code Improve API Doc",
-
-};
-
- module.exports = { swaggerServe: swaggerUI.serve, swaggerSetup: swaggerUI.setup(swaggerJSDocs) };
-
->>>>>>> origin/main
 // module.exports = { swaggerServe: swaggerUI.serve, swaggerSetup: swaggerUI.setup(swaggerJSDocs,options) };
